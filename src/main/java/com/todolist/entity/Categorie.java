35a75b3f0005f01ea0ecf43a678cafc9f339438e
epiphanie8 <epiphanieatta8@gmail.com>
2024-10-30 08:24:53 +0000

    finish configuration et data base migration

---
+++ src/main/java/com/todolist/entity/Categorie.java
@@ -0,0 +1,26 @@
+package com.todolist.entity;
+
+import java.util.ArrayList;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Categorie {
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String libelle_cat;
+	
+	@OneToMany(mappedBy = "categorie", cascade = CascadeType.ALL, orphanRemoval = true)
+	private ArrayList<Tache> tache;
+}
---
+++ src/main/resources/db/migration/V2__create_categorie_table.sql
@@ -0,0 +1,4 @@
+CREATE TABLE categorie(
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    libelle_cat VARCHAR(50)
+)
